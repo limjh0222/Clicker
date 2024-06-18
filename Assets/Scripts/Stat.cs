ab1b884ef197e963f3dffe6538d8d44931131f9e
limjh0222 <limjh98222@gmail.com>
2024-06-18 09:22:10 +0900

    [Add] Stat 스크립트 추가

---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Stat
+{
+    public float waitSeconds { get; set; } = 2f;
+    public int autoCount { get; set; } = 1;
+    public int clickCount { get; set; } = 1;
+}
